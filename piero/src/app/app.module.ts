5ff11c70f932abdf53a126fb134d8bbcf4ca85a6
StuderRiccardo <studer.riccardo@iisgalvanimi.edu.it>
2022-10-12 12:58:37 +0000

    quasi finito

--- piero/src/app/app.module.ts
+++ piero/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AlbertoSordiComponent } from './alberto-sordi/alberto-sordi.component';
 import { CarsComponent } from './cars/cars.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CarsComponent } from './cars/cars.component';
     CarsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
